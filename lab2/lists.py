7f839a2885ee827c38489baf63ccb260f14a7db4
Elana <156778432+Evgust@users.noreply.github.com>
2024-01-31 21:51:08 +0600

    lab2
    its i'm the problem its mee

---
+++ lab2/lists.py
@@ -0,0 +1,28 @@
+#Ex1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+#Ex2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+print(fruits)
+#Ex3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+#Ex4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+print(fruits)
+#Ex5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+#Ex6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+#Ex7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+#Ex8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
